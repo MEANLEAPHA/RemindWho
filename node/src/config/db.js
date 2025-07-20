393df2dca430ff98d48638f584a3802d5cd0f47c
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2025-07-20 14:06:33 +0700

    Change the db.js user,password,database to vlaue of the hostinger that provide

--- node/src/config/db.js
+++ node/src/config/db.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 const dbconfig = mysql.createPool(
     {
         host: "127.0.0.1",
-        user:"root", //root
-        password: '', //password
-        database:"remindme", //database
+        user:"Meanleap_Ha", //root
+        password: 'Leap2005333', //password
+        database:"u523916255_remindme", //database
         waitForConnections: true, //wait for connections
         connectionLimit: 10, //max connections
         port: 3306, //port
