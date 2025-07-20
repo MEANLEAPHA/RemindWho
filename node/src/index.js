b63b4289d35c14d2e8dca68f5a9d4de7ac5f83c4
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2025-07-20 21:01:06 +0700

    i move all html file, assets, img to puclic folder in the node folder. also i change some code in index.js using express.static point to index.html cuz the host web is lost 404 on client side

--- node/src/index.js
+++ node/src/index.js
@@ -7,6 +7,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// ✅ Serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ✅ Handle root path and send index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Import Routes
 const { ToDo } = require('./router/toDo');
 
