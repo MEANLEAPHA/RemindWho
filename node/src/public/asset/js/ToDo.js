e1e63196785b01cecb1c68d01ecb7e1516e0d844
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2025-07-21 08:56:42 +0700

    just forget to change the http and localhost now i fix it just fine

--- node/src/public/asset/js/ToDo.js
+++ node/src/public/asset/js/ToDo.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
     function LoadTask() {
         $.ajax({
-            url: 'http://localhost:3000/ToDo/remindme/displayAll',
+            url: 'https://remindwho.onrender.com/ToDo/remindme/displayAll',
             method: 'GET',
             headers: {
             'Authorization': 'Bearer ' + localStorage.getItem('token')  // ← Token added here
