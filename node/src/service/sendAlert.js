37b4d6ca65b5506ecdfba3f4ded6d0d3c4ea556d
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-06-23 14:35:41 +0700

    now that it

--- node/src/service/sendAlert.js
+++ node/src/service/sendAlert.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
     secure: true,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
+        pass: process.env.EMAIL_PASS
     }
 });
 
