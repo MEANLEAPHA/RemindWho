d73278f14b368a6ec398ae1a414d06eeec720e1f
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-07-21 08:53:53 +0700

    del port

--- node/src/service/sendAlert.js
+++ node/src/service/sendAlert.js
@@ -7,8 +7,8 @@ const db = require('../config/db');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
